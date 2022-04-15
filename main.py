d83eb4510b2114c99f338a24241d0a7e2f94641e
Nattawatt Hongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2022-04-15 20:36:04 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import names
+import requests
+from colorama import *
+url_base = 'https://github.com/'
+
+
+amount = int(input(Fore.LIGHTRED_EX + 'Enter the number of users that do you want to gen >  '))
+print("Powered By nattawatt#8556")
+
+for i in range(amount):
+    url = url_base + (names.get_first_name())
+    r = requests.get(url)
+    
+
+    if r.status_code == 200:
+        print(Fore.GREEN + "[Valid] => " + Fore.RESET + url)
+        open('github.log', 'a+').write(url + "\n")
+    else:
+        print(Fore.RED + "[Invalid] => " + Fore.RESET + url , r.status_code)
